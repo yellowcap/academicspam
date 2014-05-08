c48bfc055510c7349cd4ebca216bddac5f2ec62d
Daniel <daniel@courbanize.com>
2014-05-08 17:29:10 +0100

    Heroku adoptions

--- academicspam/settings.py
+++ academicspam/settings.py
@@ -52,16 +52,16 @@ LOGIN_URL = '/login/'
 
 
 ########## DATABASE CONFIGURATION
-DATABASES = {
-    'default': {
-        'ENGINE':   os.environ.get('DB_ENGINE'),
-        'NAME':     os.environ.get('DB_NAME'),
-        'USER':     os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST':     os.environ.get('DB_HOST'),
-        'PORT':     os.environ.get('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE':   os.environ.get('DB_ENGINE'),
+#         'NAME':     os.environ.get('DB_NAME'),
+#         'USER':     os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST':     os.environ.get('DB_HOST'),
+#         'PORT':     os.environ.get('DB_PORT'),
+#     }
+# }
 ########## END DATABASE CONFIGURATION
 
 ########## MIDDLEWARE CONFIGURATION
