d3b2070a0dda67e2f036133852a3a27d96dcd96d
Daniel <daniel@courbanize.com>
2014-05-08 18:08:40 +0100

    Trying to remove gis

--- academicspam/settings.py
+++ academicspam/settings.py
@@ -116,7 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
-    'django.contrib.gis',
+    #'django.contrib.gis',
 
     # Own apps
     'spamparser',
