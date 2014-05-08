a36e984d7bdcfae4b7842ada94e7a43b91aaa191
Daniel <daniel@courbanize.com>
2014-05-08 22:52:37 +0100

    Removed heroky static settings

--- academicspam/settings.py
+++ academicspam/settings.py
@@ -291,12 +291,12 @@ if not DEBUG:
     ALLOWED_HOSTS = ['*']
 
     # Static asset configuration
-    import os
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATIC_URL = '/static/'
+    #import os
+    #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    #STATIC_ROOT = 'staticfiles'
+    #STATIC_URL = '/static/'
 
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    # STATICFILES_DIRS = (
+    #     os.path.join(BASE_DIR, 'static'),
+    # )
 
