6878b24d8d6a23c0652957654347de108689294e
Daniel <daniel@courbanize.com>
2014-05-29 17:50:41 +0100

    Added basic listing for spam emails to index page

--- academicspam/views.py
+++ academicspam/views.py
@@ -5,11 +5,19 @@ from django.template import RequestContext, loader
 #from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from spamparser.models import ParseResult
+
 ###############################################################################
 @login_required
 def index(request):
     """View for index page of academicspam"""
 
+    pres = ParseResult.objects.filter()[:10]
+    count = ParseResult.objects.filter().count()
+
     template = loader.get_template('index.html')
-    context = RequestContext(request)
+    context = RequestContext(request, {
+        'parseresults': pres,
+        'parsecount': count
+    })
     return HttpResponse(template.render(context))
